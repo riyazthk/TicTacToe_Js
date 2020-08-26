6e1f71598be110a761a2d2725356d785fb6f1c05
riyazthk <er.riyaz2507@gmail.com>
2020-08-26 09:17:06 +0530

    [Riyaz]ADDED:To write a code to determine a variable for playing a game

--- TicTacToe_Js/app.js
+++ TicTacToe_Js/app.js
@@ -1,10 +1,36 @@
+var readlineSync = require('readline-sync');
 var board = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
 var head = 1
-var tail = 2
-var toss = Math.floor(Math.random() * 2) + 1;
+var userVariable
+var computerVariable
+var toss = calculateToss()
+console.log(toss)
+function calculateToss() {
+    return Math.floor(Math.random() * 2) + 1;
+}
+function calculateComputerVariable() {
+    if (userVariable === "x") {
+        computerVariable = "o"
+    } else {
+        computerVariable="x"
+    }
+}
 if (toss === head) {
     console.log("user won a toss")
+    console.log("choose a letter x or o")
+    userVariable = readlineSync.question("enter a letter")
+    calculateComputerVariable()
 } else {
     console.log("computer won a toss")
+    var result = calculateToss()
+    if (result == 1) {
+        computerVariable = "o"
+        userVariable="x"
+    } else {
+        computerVariable = "x"
+        userVariable="o"
+    }
 }
+console.log("userVariable "+userVariable)
+console.log("computerVariable "+computerVariable)
 
